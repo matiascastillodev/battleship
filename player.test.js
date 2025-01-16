e023ec2c743bc5b150d2719a80192194ed8a0333
matiascastilloaudio <177944762+matiascastilloaudio@users.noreply.github.com>
2025-01-16 13:36:25 -0300

    fix: replace variable name

--- player.test.js
+++ player.test.js
@@ -3,23 +3,23 @@ import { Ship } from "./ship.js";
 
 test("player can attack enemy's gameboard", () => {
   const player = new Player();
-  const enemy = new Player();
+  const computer = new Player();
 
   const ship = new Ship(3);
-  enemy.gameboard.placeShip(ship, [0, 0], "horizontal");
+  computer.gameboard.placeShip(ship, [0, 0], "horizontal");
 
-  const result = player.attack(enemy.gameboard, [0, 0]);
+  const result = player.attack(computer.gameboard, [0, 0]);
   expect(result).toBe("hit");
   expect(ship.hits).toBe(1);
 });
 
 test("computer makes a valid random attack", () => {
+  const player = new Player();
   const computer = new Player();
-  const enemy = new Player();
 
   const ship = new Ship(3);
-  enemy.gameboard.placeShip(ship, [0, 0], "horizontal");
+  player.gameboard.placeShip(ship, [0, 0], "horizontal");
 
-  const result = computer.randomAttack(enemy.gameboard);
+  const result = computer.randomAttack(player.gameboard);
   expect(["hit", "miss"]).toContain(result);
 });
